60d7f53335db778b9746251c2249f444941396c6
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2011-08-18 18:15:31 +0200

    Init commit, 0.1 version.

---
+++ index.js
@@ -0,0 +1,61 @@
+/*!
+ * Node.JS module "Deep Extend" for recursive object extending.
+ * @author Viacheslav Lotsmanov lotsmanov89 at gmail dot com
+ * @version 0.1
+ */
+
+/**
+ * Extening object that entered in first argument.
+ * Returns extended object or false if have no target object or incorrect type.
+ * If you wish to clone object, simply use that:
+ *   deepExtend({}, yourObj_1, [yourObj_N]) - first arg is new empty object
+ */
+var deepExtend = function (/*obj_1, [obj_2], [obj_N]*/) {
+	var target, key, val, src, clone, args = [], i = 1;
+	
+	if (arguments.length < 1 || typeof arguments[0] !== 'object') {
+		return false;
+	}
+	
+	target = arguments[0];
+	
+	for (; i < arguments.length; i++) {
+		args.push(arguments[i]);
+	}
+	
+	if (args.length > 0) {
+		args.forEach(function (obj) {
+			if (typeof obj !== 'object') {
+				return;
+			}
+			
+			for (key in obj) {
+				if (obj[key] !== void 0) {
+					src = target[key];
+					val = obj[key];
+					
+					if (val === target) {
+						continue;
+					}
+					
+					if (typeof val !== 'object') {
+						target[key] = val;
+						continue;
+					}
+					
+					if (Array.isArray(val)) {
+						clone = (Array.isArray(src)) ? src : [];
+					} else {
+						clone = (!Array.isArray(src)) ? src : {};
+					}
+					
+					target[key] = deepExtend(clone, val);
+				}
+			}
+		});
+	}
+	
+	return target;
+}
+
+module.exports = deepExtend;
