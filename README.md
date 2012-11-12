ee3c3f22c6cc8c0c8f47854e602be724ffd07412
unclechu <unclechu@unclechu-xubuntu.(none)>
2012-11-12 13:05:33 +0400

    Fixed bug: converting null to empty object.

--- README.md
+++ README.md
@@ -5,52 +5,52 @@ Recursive object extending.
 
 Version
 -----
-0.2.2
+0.2.3
 
 Install
 -----
 
-	npm install deep-extend
+    npm install deep-extend
 
 Usage
 -----
 
-	var deepExtend = require('deep-extend'),
-		obj1 = {
-			a: 1,
-			b: 2,
-			d: {
-				a: 1,
-				b: [],
-				c: {test1: 123, test2: 321}
-			},
-			f: 5
-		},
-		obj2 = {
-			b: 3,
-			c: 5,
-			d: {
-				b: {first: 'one', second: 'two'},
-				c: {test2: 222}
-			},
-			e: {one: 1, two: 2},
-			f: []
-		};
-	
-	deepExtend(obj1, obj2);
-	
-	console.log(obj1);
-	/*
-		{
-			a: 1,
-			b: 3,
-			d: {
-				a: 1,
-				b: {first: 'one', second: 'two'},
-				c: {test1: 123, test2: 222} 
-			},
-			f: [],
-			c: 5,
-			e: {one: 1, two: 2}
-		}
-	*/
+    var deepExtend = require('deep-extend'),
+        obj1 = {
+            a: 1,
+            b: 2,
+            d: {
+                a: 1,
+                b: [],
+                c: {test1: 123, test2: 321}
+            },
+            f: 5
+        },
+        obj2 = {
+            b: 3,
+            c: 5,
+            d: {
+                b: {first: 'one', second: 'two'},
+                c: {test2: 222}
+            },
+            e: {one: 1, two: 2},
+            f: []
+        };
+    
+    deepExtend(obj1, obj2);
+    
+    console.log(obj1);
+    /*
+        {
+            a: 1,
+            b: 3,
+            d: {
+                a: 1,
+                b: {first: 'one', second: 'two'},
+                c: {test1: 123, test2: 222} 
+            },
+            f: [],
+            c: 5,
+            e: {one: 1, two: 2}
+        }
+    */
--- index.js
+++ index.js
@@ -11,55 +11,55 @@
  *  deepExtend({}, yourObj_1, [yourObj_N]) - first arg is new empty object
  */
 module.exports = deepExtend = function (/*obj_1, [obj_2], [obj_N]*/) {
-	var target, key, val, src, clone, args = [], i = 1;
-	
-	if (arguments.length < 1 || typeof arguments[0] !== 'object') {
-		return false;
-	}
-	
-	target = arguments[0];
-	
-	for (; i < arguments.length; i++) {
-		args.push(arguments[i]);
-	}
-	
-	if (args.length > 0) {
-		args.forEach(function (obj) {
-			if (typeof obj !== 'object') {
-				return;
-			}
-			
-			for (key in obj) {
-				if (obj[key] !== void 0) {
-					src = target[key];
-					val = obj[key];
-					
-					if (val === target) {
-						continue;
-					}
-					
-					if (typeof val !== 'object') {
-						target[key] = val;
-						continue;
-					}
-					
-					if (typeof src !== 'object') {
-						clone = (Array.isArray(val)) ? [] : {};
-						target[key] = deepExtend(clone, val);
-						continue;
-					}
-					
-					if (Array.isArray(val)) {
-						clone = (Array.isArray(src)) ? src : [];
-					} else {
-						clone = (!Array.isArray(src)) ? src : {};
-					}
-					
-					target[key] = deepExtend(clone, val);
-				}
-			}
-		});
-	}
-	
-	return target;
+    var target, key, val, src, clone, args = [], i = 1;
+    
+    if (arguments.length < 1 || typeof arguments[0] !== 'object') {
+        return false;
+    }
+    
+    target = arguments[0];
+    
+    for (; i < arguments.length; i++) {
+        args.push(arguments[i]);
+    }
+    
+    if (args.length > 0) {
+        args.forEach(function (obj) {
+            if (typeof obj !== 'object') {
+                return;
+            }
+            
+            for (key in obj) {
+                if (obj[key] !== void 0) {
+                    src = target[key];
+                    val = obj[key];
+                    
+                    if (val === target) {
+                        continue;
+                    }
+                    
+                    if (typeof val !== 'object' || val === null) {
+                        target[key] = val;
+                        continue;
+                    }
+                    
+                    if (typeof src !== 'object') {
+                        clone = (Array.isArray(val)) ? [] : {};
+                        target[key] = deepExtend(clone, val);
+                        continue;
+                    }
+                    
+                    if (Array.isArray(val)) {
+                        clone = (Array.isArray(src)) ? src : [];
+                    } else {
+                        clone = (!Array.isArray(src)) ? src : {};
+                    }
+                    
+                    target[key] = deepExtend(clone, val);
+                }
+            }
+        });
+    }
+    
+    return target;
 }
