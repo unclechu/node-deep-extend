189924cb07e3755d8757b027b0ce1e29649a58b2
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2014-07-28 21:16:24 +0400

    support Date objects

--- index.js
+++ index.js
@@ -33,55 +33,58 @@
  *  deepExtend({}, yourObj_1, [yourObj_N]) - first arg is new empty object
  */
 var deepExtend = module.exports = function (/*obj_1, [obj_2], [obj_N]*/) {
-    if (arguments.length < 1 || typeof arguments[0] !== 'object') {
-        return false;
-    }
+	if (arguments.length < 1 || typeof arguments[0] !== 'object') {
+		return false;
+	}
 
-    if (arguments.length < 2) return arguments[0];
+	if (arguments.length < 2) return arguments[0];
 
-    var target = arguments[0];
+	var target = arguments[0];
 
-    // convert arguments to array and cut off target object
-    var args = Array.prototype.slice.call(arguments, 1);
+	// convert arguments to array and cut off target object
+	var args = Array.prototype.slice.call(arguments, 1);
 
-    var key, val, src, clone, tmpBuf;
+	var key, val, src, clone, tmpBuf;
 
-    args.forEach(function (obj) {
-        if (typeof obj !== 'object') return;
+	args.forEach(function (obj) {
+		if (typeof obj !== 'object') return;
 
-        for (key in obj) {
-            if ( ! (key in obj)) continue;
+		for (key in obj) {
+			if ( ! (key in obj)) continue;
 
-            src = target[key];
-            val = obj[key];
+			src = target[key];
+			val = obj[key];
 
-            if (val === target) continue;
+			if (val === target) continue;
 
-            if (typeof val !== 'object' || val === null) {
-                target[key] = val;
-                continue;
-            } else if (val instanceof Buffer) {
-                tmpBuf = new Buffer(val.length);
-                val.copy(tmpBuf);
-                target[key] = tmpBuf;
-                continue;
-            }
+			if (typeof val !== 'object' || val === null) {
+				target[key] = val;
+				continue;
+			} else if (val instanceof Buffer) {
+				tmpBuf = new Buffer(val.length);
+				val.copy(tmpBuf);
+				target[key] = tmpBuf;
+				continue;
+			} else if (val instanceof Date) {
+				target[key] = new Date(val.getTime());
+				continue;
+			}
 
-            if (typeof src !== 'object' || src === null) {
-                clone = (Array.isArray(val)) ? [] : {};
-                target[key] = deepExtend(clone, val);
-                continue;
-            }
+			if (typeof src !== 'object' || src === null) {
+				clone = (Array.isArray(val)) ? [] : {};
+				target[key] = deepExtend(clone, val);
+				continue;
+			}
 
-            if (Array.isArray(val)) {
-                clone = (Array.isArray(src)) ? src : [];
-            } else {
-                clone = (!Array.isArray(src)) ? src : {};
-            }
+			if (Array.isArray(val)) {
+				clone = (Array.isArray(src)) ? src : [];
+			} else {
+				clone = (!Array.isArray(src)) ? src : {};
+			}
 
-            target[key] = deepExtend(clone, val);
-        }
-    });
+			target[key] = deepExtend(clone, val);
+		}
+	});
 
-    return target;
+	return target;
 }
